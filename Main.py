1cb949f7c8aba61b0d903607e408a37477219eb0
sergioperez18 <92035886+sergioperez18@users.noreply.github.com>
2024-09-16 22:25:29 +0200

    Add files via upload

---
+++ Main.py
@@ -0,0 +1,9 @@
+import streamlit as st
+import pandas as pd
+ 
+st.write("""
+Sergio Pérez"""
+)
+#use tis link https://www.google.com/url?sa=i&url=https%3A%2F%2Fes.linkedin.com%2Fcompany%2Fsdg-group&psig=AOvVaw1AqTLraM0T-ebiS4Jf00ka&ust=1726588987559000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCIiA-a3rx4gDFQAAAAAdAAAAABAE
+#put phto on the left
+st.image("https://www.google.com/url?sa=i&url=https%3A%2F%2Fes.linkedin.com%2Fcompany%2Fsdg-group&psig=AOvVaw1AqTLraM0T-ebiS4Jf00ka&ust=1726588987559000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCIiA-a3rx4gDFQAAAAAdAAAAABAE", width=200)
